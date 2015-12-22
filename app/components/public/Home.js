bb15b2f130dd05e8b0cdc0482dd99d24441a2c09
Arne Zeising <arne.zeising@gmail.com>
2015-12-22 22:21:38 +0100

    Update input field

--- app/components/public/Home.js
+++ app/components/public/Home.js
@@ -13,8 +13,6 @@ const Home = React.createClass({
     },
 
     handleInputChange: function() {
-      UserActions.setType('beach');
-      this.context.router.transitionTo('week');
       console.log(inputValue)
       if (inputValue.toUpperCase() == "SNOW") {
         this.context.router.transitionTo('result');
@@ -25,19 +23,17 @@ const Home = React.createClass({
         var inputValue = this.state.value;
         return (
             <div className="row section-type">
-                <div className="col s12 section-type__choice section-type__choice--beach">
-                  <h1>
-                    Feathers, I am as light,
-                    But quite heavy if squeezed tight.
-                    I am seen as clean and pure,
-                    but often talked about with dirty terms.
-                    What am I?
-                  </h1>
+                <div className="col s12">
+                    <h1>
+                      Feathers, I am as light,
+                      But quite heavy if squeezed tight.
+                      I am seen as clean and pure,
+                      but often talked about with dirty terms.
+                      What am I?
+                    </h1>
                 </div>
-                <div className="col s12 section-type__choice section-type__choice--beach">
-                    <div onClick={this.handleClickBeach} className="section-type__choice__bg">
-                        <input type="text" value={inputValue} onChange={this.handleInputChange} defaultValue="Answer!" />
-                    </div>
+                <div className="col s12">
+                    <input type="text" value={inputValue} onChange={this.handleInputChange} defaultValue="Answer!" />
                 </div>
             </div>
         );
